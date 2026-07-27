3714521ff20fa6c12177cc65e087b04a84213043
Elaf-Muneeb453 <elafmuneeb1@gmail.com>
2026-07-27 15:32:48 +0500

    update

--- database.py
+++ database.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import psycopg 
 
+hostname = "localhost"
+port=5000
+database_name="weather_forecasting"
+server_name="postgres"
+pwd ="123789"
+
+
 conn = psycopg.connect(
-    
+    host=hostname,
+    port=5000,
+    dbname=database_name,  # or another existing database
+    user=server_name,
+    password=pwd,
+    autocommit=True,
 )
