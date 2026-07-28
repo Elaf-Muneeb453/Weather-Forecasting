504b26760725744cd9b97ddb89288ad7c7eca762
Elaf-Muneeb453 <elafmuneeb1@gmail.com>
2026-07-28 17:57:21 +0500

    update

---
+++ store_data_site_weather.py
@@ -0,0 +1,33 @@
+import psycopg
+from weather_api import get_data
+
+def data_store():
+
+    sites_df = get_data()
+
+    with psycopg.connect(
+        dbname="site_weather", 
+        user="postgres", 
+        password="123789",
+        host="localhost", 
+        port="5000"
+    ) as conn:
+
+        with conn.cursor() as cur:
+
+            for _, row in sites_df.iterrows():
+
+                cur.execute(
+                    """
+                    INSERT INTO site_weather
+                    (site_name, time_interval, temperature, humidity, solar_radiance)
+                    VALUES (%s, %s, %s, %s, %s)
+                    """,
+                    (row["site_code"], row["time_interval"], row["temperature"], row["humidity"], row["solar_radiance"]),
+                )
+
+        conn.commit()
+
+    print("Sites inserted successfully!")
+
+data_store()
--- weather_api.py
+++ weather_api.py
@@ -1,58 +1,3 @@
-# import openmeteo_requests
-
-# import pandas as pd
-# import requests_cache
-# from retry_requests import retry
-# from read_metadata_table import get_sites
-
-
-# # Setup the Open-Meteo API client with cache and retry on error
-# cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
-# retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
-# openmeteo = openmeteo_requests.Client(session=retry_session)
-
-# # Make sure all required weather variables are listed here
-# # The order of variables in hourly or daily is important to assign them correctly below
-# url = "https://api.open-meteo.com/v1/forecast"
-# params = {
-#     "latitude": 52.52,
-#     "longitude": 13.41,
-#     "hourly": ["temperature_2m", "relative_humidity_2m", "direct_radiation_instant"],
-#     "start_date": "2026-07-20",
-#     "end_date": "2026-07-28",
-# }
-# responses = openmeteo.weather_api(url, params=params)
-
-# # Process first location. Add a for-loop for multiple locations or weather models
-# response = responses[0]
-# print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
-# print(f"Elevation: {response.Elevation()} m asl")
-# print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
-
-# # Process hourly data. The order of variables needs to be the same as requested.
-# hourly = response.Hourly()
-# hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
-# hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
-# hourly_direct_radiation_instant = hourly.Variables(2).ValuesAsNumpy()
-
-# hourly_data = {
-#     "date": pd.date_range(
-#         start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-#         end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-#         freq=pd.Timedelta(seconds=hourly.Interval()),
-#         inclusive="left",
-#     )
-# }
-
-# hourly_data["temperature_2m"] = hourly_temperature_2m
-# hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
-# hourly_data["direct_radiation_instant"] = hourly_direct_radiation_instant
-
-# hourly_dataframe = pd.DataFrame(data=hourly_data)
-# hourly_dataframe.to_csv("eight_das.csv")
-# print("\nHourly data\n", hourly_dataframe)
-
-
 import openmeteo_requests
 import pandas as pd
 import requests_cache
@@ -63,7 +8,6 @@ from read_metadata_table import get_sites
 cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
 retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 openmeteo = openmeteo_requests.Client(session=retry_session)
-
 
 def get_weather(site_code, latitude, longitude):
 
@@ -102,28 +46,28 @@ def get_weather(site_code, latitude, longitude):
             ),
             "temperature": temperature,
             "humidity": humidity,
-            "solar_radiation": radiation,
+            "solar_radiance": radiation,
         }
     )
-
+    
     return hourly_dataframe
 
-sites = get_sites()
+def get_data():
+    sites = get_sites()
+    all_weather_data = []
+    
+    for site_code, latitude, longitude in sites:
 
-all_weather_data = []
+        # print(f"Fetching weather for {site_code}")
+        weather_df = get_weather(site_code, latitude, longitude)
+        
+        all_weather_data.append(weather_df)
 
+    final_dataframe = pd.concat(all_weather_data, ignore_index=True)
+    return final_dataframe
 
-for site_code, latitude, longitude in sites:
+# print(type(get_data()))
+# print(final_dataframe)
 
-    print(f"Fetching weather for {site_code}")
-    weather_df = get_weather(site_code, latitude, longitude)
-    all_weather_data.append(weather_df)
-
-
-final_dataframe = pd.concat(all_weather_data, ignore_index=True)
-
-
-print(final_dataframe)
-
-# Optional save
-final_dataframe.to_csv("all_sites_weather.csv", index=False)
+# # Optional save
+# final_dataframe.to_csv("all_sites_weather.csv", index=False)
