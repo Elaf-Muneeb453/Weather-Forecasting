a6c917d4662498c44f3976542b8d76500afc3df9
Elaf-Muneeb453 <elafmuneeb1@gmail.com>
2026-07-28 12:56:02 +0500

    update

--- database.py
+++ database.py
@@ -2,12 +2,8 @@ import psycopg
 from psycopg import sql
 import requests
 
-
-
 def create_db_meta(db_name):
     try:
-        # 1. Connect to the default system database 'postgres'
-        # 2. Explicitly set autocommit=True to prevent automatic transaction blocks
         with psycopg.connect(
             dbname="csv_database",
             user="postgres",
@@ -16,9 +12,7 @@ def create_db_meta(db_name):
             port="5000",
             autocommit=True,
         ) as conn:
-
             with conn.cursor() as cur:
-                # Use the sql module to safely build the query and avoid SQL injection
                 query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name))
                 cur.execute(query)
                 print(f"Database '{db_name}' created successfully!")
@@ -29,8 +23,6 @@ def create_db_meta(db_name):
 
 def create_db_site_weather(db_name):
     try:
-        # 1. Connect to the default system database 'postgres'
-        # 2. Explicitly set autocommit=True to prevent automatic transaction blocks
         with psycopg.connect(
             dbname="csv_database",
             user="postgres",
@@ -41,7 +33,6 @@ def create_db_site_weather(db_name):
         ) as conn:
 
             with conn.cursor() as cur:
-                # Use the sql module to safely build the query and avoid SQL injection
                 query = sql.SQL("CREATE DATABASE {}").format(sql.Identifier(db_name))
                 cur.execute(query)
                 print(f"Database '{db_name}' created successfully!")
@@ -50,7 +41,62 @@ def create_db_site_weather(db_name):
         print(f"An error occurred: {e}")
 
 
+def create_table_meta():
+    try:
+        with psycopg.connect(
+            dbname="meta",
+            user="postgres",
+            password="123789",
+            host="localhost",
+            port="5000",
+        ) as conn:
+
+            with conn.cursor() as cur:
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS metadata (
+                        site_name VARCHAR(100) Not NULL,
+                        latitude DOUBLE PRECISION Not NULL,
+                        longitude DOUBLE PRECISION Not NULL
+                    );
+                """)
+
+            conn.commit()
+            print("metadata table created.")
+
+    except psycopg.Error as e:
+        print(e)
+
+
+def create_table_site_weather():
+    try:
+        with psycopg.connect(
+            dbname="site_weather",
+            user="postgres",
+            password="123789",
+            host="localhost",
+            port="5000",
+        ) as conn:
+
+            with conn.cursor() as cur:
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS site_weather (
+                        site_name VARCHAR(100) Not NULL,
+                        time_interval TIMESTAMPTZ Not NULL,
+                        temperature REAL Not NULL,
+                        humidity REAL Not NULL,
+                        solar_radiace REAL Not NULL
+                    );
+                """)
+
+            conn.commit()
+            print("site_weather table created.")
+
+    except psycopg.Error as e:
+        print(e)
+
+
 if __name__ == "__main__":
-    create_db_meta("meta")
-    create_db_site_weather("site_weather")
-    
+    # create_db_meta("meta")
+    # create_db_site_weather("site_weather")
+    create_table_meta()
+    create_table_site_weather()
