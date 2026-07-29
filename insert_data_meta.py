92e6b5b815a2816351dd89bd3c663be160b51bad
Elaf-Muneeb453 <elafmuneeb1@gmail.com>
2026-07-29 16:49:20 +0500

    update

--- insert_data_meta.py
+++ insert_data_meta.py
@@ -3,17 +3,16 @@ from random_site_code import generate_site_data
 
 
 def insert_sites():
-
+     
     sites_df = generate_site_data(100)
-
+    
     with psycopg.connect(
         dbname="meta", 
         user="postgres", 
         password="123789", 
         host="localhost", 
         port="5000"
-    ) as conn:
-
+    ) as conn: 
         with conn.cursor() as cur:
             for _, row in sites_df.iterrows():
                 
@@ -21,7 +20,7 @@ def insert_sites():
                     """
                     INSERT INTO metadata
                     (site_name, latitude, longitude)
-                    VALUES (%s, %s, %s)
+                    VALUES (%s, %s, %s)                                                                         
                     """,
                     (row["site_code"], row["latitude"], row["longitude"]),
                 )
